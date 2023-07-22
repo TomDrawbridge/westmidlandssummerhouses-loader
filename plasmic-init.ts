482c754ac713c4da17efc1a3dc4fc067013da0f7
TomDrawbridge <tom.prest@drawbridgelabs.co.uk>
2023-07-22 10:56:01 +0100

    Test 123

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,3 +1,6 @@
+/** @format */
+import { ScrollProvider } from "./components/ScrollContext";
+
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
 
 export const PLASMIC = initPlasmicLoader({
@@ -12,7 +15,7 @@ export const PLASMIC = initPlasmicLoader({
   // For development, you can set preview to true, which will use the unpublished
   // project, allowing you to see your designs without publishing.  Please
   // only use this for development, as this is significantly slower.
-  preview: false,
+  preview: true,
 });
 
 // You can register any code components that you want to use here; see
@@ -22,4 +25,8 @@ export const PLASMIC = initPlasmicLoader({
 // http://localhost:3000/plasmic-host).  See
 // https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
 
-// PLASMIC.registerComponent(...);
+PLASMIC.registerGlobalContext(ScrollProvider, {
+  name: "ScrollProvider",
+  providesData: true,
+  props: {},
+});
