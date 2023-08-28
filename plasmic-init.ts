fcf3f4c5103f3ba28f8d75169cc7f5f6a52d89ed
TomDrawbridge <tom.prest@drawbridgelabs.co.uk>
2023-08-28 22:05:58 +0100

    added markdown component

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -82,7 +82,7 @@ className: 'string',
   },
 });
 
-import ReactMarkdown from "./components/ReactMarkdown";
+import ReactMarkdownComponent from "./components/ReactMarkdown";
 
 PLASMIC.registerComponent(ReactMarkdownComponent, {
   name: "ReactMarkdown",
