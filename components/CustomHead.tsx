97cbd53e0a273714fe546e9e7a5200bc3fe0a557
TomDrawbridge <tom.prest@drawbridgelabs.co.uk>
2023-10-25 13:17:33 +0100

    added head html component

--- components/CustomHead.tsx
+++ components/CustomHead.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Head from 'next/head';
 import parse from 'html-react-parser';
 
+interface CustomHeadProps {
+  mycodehere: string;
+}
+
 const CustomHead = ({ mycodehere }) => {
   return (
 <Head>
