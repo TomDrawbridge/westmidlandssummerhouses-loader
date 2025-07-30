83f15fa10880591a7565aaef33098129df6335a1
TomDrawbridge <139471610+TomDrawbridge@users.noreply.github.com>
2025-07-30 17:41:15 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -54,32 +54,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // Apply headers to all routes
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block'
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin'
-          }
-        ],
-      },
-      {
         // Cache static assets
         source: '/fonts/(.*)',
         headers: [
